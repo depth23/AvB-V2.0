eb22c41a07a78713689bc7e752c4a33685b6f1ab
depth23 <deon.p.sharps@gmail.com>
2016-05-05 16:21:35 -0600

    added designed by, added site nav and auto scroll to footer, added to top tab and auto scroll

--- public/JS/app.js
+++ public/JS/app.js
@@ -16,30 +16,44 @@ function autoScrollTo(el) {
 	if(yPos > bodyHeight){
 		clearTimeout(animator);
 	} else {
-		if(currentY < targetY-distance){
-		    scrollY = currentY+distance;
-		    window.scroll(0, scrollY);
+		if(currentY < targetY - distance){
+		    scrollY = currentY + distance;
+		    window.scroll(0, scrollY + distance);
 	    } else {
 		    clearTimeout(animator);
 	    }
 	}
 }
 
-//function resetScroller(el){
-//	var currentY = window.pageYOffset;
-//    var targetY = document.getElementById(el).offsetTop;
-//	var animator = setTimeout('resetScroller(\''+el+'\')',speed);
-//	if(currentY > targetY){
-//		scrollY = currentY-distance;
-//		window.scroll(0, scrollY);
-//	} else {
-//		clearTimeout(animator);
-//	}
-//}
+function resetScroller(el){
+	var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+	var animator = setTimeout('resetScroller(\''+el+'\')',speed);
+	if(currentY > targetY){
+		scrollY = currentY - distance;
+		window.scroll(0, scrollY);
+	} else {
+		clearTimeout(animator);
+	}
+}
 
 //--------------------Hamburger Menu---------------------
 
-document.getElementById('hamburger').addEventListener('click', function() {
+function section1Click() {
+  document.getElementById('hamburger').style.display = "inline-block";
+  document.getElementById('pop-out-one').style.transition = "right 1s";
+  document.getElementById('pop-out-one').style.right = "-13rem";
+  document.getElementById('pop-out-two').style.transition = "right 1.25s";
+  document.getElementById('pop-out-two').style.right = "-13rem";
+  document.getElementById('pop-out-three').style.transition = "right 1.5s";
+  document.getElementById('pop-out-three').style.right = "-13rem";
+  document.getElementById('pop-out-four').style.transition = "right 1.75s";
+  document.getElementById('pop-out-four').style.right = "-13rem";
+};
+
+document.getElementById('sectionOne').addEventListener('click', section1Click, true);
+
+function hamburgerClick() {
   document.getElementById('hamburger').style.display = "none";
   document.getElementById('pop-out-one').style.transition = "right 1s";
   document.getElementById('pop-out-one').style.right = "1rem";
@@ -49,9 +63,11 @@ document.getElementById('hamburger').addEventListener('click', function() {
   document.getElementById('pop-out-three').style.right = "1rem";
   document.getElementById('pop-out-four').style.transition = "right 1.75s";
   document.getElementById('pop-out-four').style.right = "1rem";
-}, false);
+};
 
-document.getElementById('pop-out-one').addEventListener('click', function() {
+document.getElementById('hamburger').addEventListener('click', hamburgerClick, false);
+
+function popOutOneClick() {
   document.getElementById('hamburger').style.display = "inline-block";
   document.getElementById('pop-out-one').style.transition = "right 1s";
   document.getElementById('pop-out-one').style.right = "-13rem";
@@ -62,9 +78,11 @@ document.getElementById('pop-out-one').addEventListener('click', function() {
   document.getElementById('pop-out-four').style.transition = "right 1.75s";
   document.getElementById('pop-out-four').style.right = "-13rem";
   autoScrollTo('menuSection');
-}, false);
+}
 
-document.getElementById('pop-out-two').addEventListener('click', function() {
+document.getElementById('pop-out-one').addEventListener('click', popOutOneClick, false);
+
+function popOutTwoClick() {
   document.getElementById('hamburger').style.display = "inline-block";
   document.getElementById('pop-out-one').style.transition = "right 1.75s";
   document.getElementById('pop-out-one').style.right = "-13rem";
@@ -75,9 +93,11 @@ document.getElementById('pop-out-two').addEventListener('click', function() {
   document.getElementById('pop-out-four').style.transition = "right 1.5s";
   document.getElementById('pop-out-four').style.right = "-13rem";
   autoScrollTo('find-us-link');
-}, false);
+}
 
-document.getElementById('pop-out-three').addEventListener('click', function() {
+document.getElementById('pop-out-two').addEventListener('click', popOutTwoClick, false);
+
+function popOutThreeClick() {
   document.getElementById('hamburger').style.display = "inline-block";
   document.getElementById('pop-out-one').style.transition = "right 1.5s";
   document.getElementById('pop-out-one').style.right = "-13rem";
@@ -88,9 +108,11 @@ document.getElementById('pop-out-three').addEventListener('click', function() {
   document.getElementById('pop-out-four').style.transition = "right 1.75s";
   document.getElementById('pop-out-four').style.right = "-13rem";
   autoScrollTo('about-link');
-}, false);
+}
 
-document.getElementById('pop-out-four').addEventListener('click', function() {
+document.getElementById('pop-out-three').addEventListener('click', popOutThreeClick, false);
+
+function popOutFourClick() {
   document.getElementById('hamburger').style.display = "inline-block";
   document.getElementById('pop-out-one').style.transition = "right 1.75s";
   document.getElementById('pop-out-one').style.right = "-13rem";
@@ -101,7 +123,9 @@ document.getElementById('pop-out-four').addEventListener('click', function() {
   document.getElementById('pop-out-four').style.transition = "right 1s";
   document.getElementById('pop-out-four').style.right = "-13rem";
   autoScrollTo('about-link');
-}, false);
+}
+
+document.getElementById('pop-out-four').addEventListener('click', popOutFourClick, false);
 
 //--------------------Sec 1 Pointer----------------------
 
@@ -140,47 +164,9 @@ document.getElementById('sec-1-share-cntnr').addEventListener('mouseleave', func
 
 //----------------------Menu-----------------------------
 
+var screenSize = window.screen.width;
+
   //Menu Top
-
-    //Hover
-
-//document.getElementById("menuTop").addEventListener('mouseover', displayMenuTop, false);
-//
-//function displayMenuTop(e) {
-//  Breakfast
-//  if(e.target !== e.currentTarget && e.target <= "bfast") {
-//    document.getElementById('sec2Logo').style.display = "none"; 
-//    document.getElementById('breakfast-full').style.display = "block"; 
-//  }
-//  Panini
-//  else if(e.target !== e.currentTarget && e.target <= "panini") {
-//    document.getElementById('sec2Logo').style.display = "none";
-//    document.getElementById('panini-full').style.display = "block"; 
-//  }
-//  Sandwiches
-//  else if() {
-//    document.getElementById('sec2Logo').style.display = "none";
-//    document.getElementById('sandwich-full').style.display = "block"; 
-//  }
-//  e.stopImmediatePropagation();
-//};
-
-//document.getElementById("menuTop").addEventListener('mouseout', HideMenuTop, false);
-//
-//function HideMenuTop() {
-  //  if() {
-//    document.getElementById('sec2Logo').style.display = "block"; 
-//    document.getElementById('breakfast-full').style.display = "none"; 
-//  }
-//  else if() {
-//    document.getElementById('sec2Logo').style.display = "block";
-//    document.getElementById('panini-full').style.display = "none"; 
-//  }
-//  else if() {
-//    document.getElementById('sec2Logo').style.display = "block";
-//    document.getElementById('sandwich-full').style.display = "none"; 
-//  }
-//}
 
 //---Breakfast---
 
@@ -206,9 +192,17 @@ document.getElementById('bfast').addEventListener('mouseout', breakfastMouseOut,
     //Click
 
 function breakfastClick() {
-  document.getElementById('breakfast-full').style.top = "16px"; 
-  document.getElementById('breakfast-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('bfastFull').style.height = "32rem";
+  if(screenSize <= 770) {
+    document.getElementById('breakfast-full').style.top = "16px"; 
+    document.getElementById('breakfast-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('bfastFull').style.height = "32rem";
+    autoScrollTo('fullMenuLink');
+  }
+  else{
+    document.getElementById('breakfast-full').style.top = "16px"; 
+    document.getElementById('breakfast-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('bfastFull').style.height = "32rem";
+  }
 };
 
 document.getElementById('bfast').addEventListener('click', breakfastClick, false);
@@ -238,13 +232,24 @@ document.getElementById('panini').addEventListener('mouseout', paniniMouseOut,fa
     //Click
 
 function paniniClick() {
-  document.getElementById('panini-full').style.top = "16px"; 
-  document.getElementById('panini-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('panini-full').style.height = "96%"
-  document.getElementById('paniniFull').style.height = "88%";
+  if(screenSize <= 770) {
+    document.getElementById('panini-full').style.top = "16px"; 
+    document.getElementById('panini-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('panini-full').style.height = "96%"
+    document.getElementById('paniniFull').style.height = "88%";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('panini-full').style.top = "16px"; 
+    document.getElementById('panini-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('panini-full').style.height = "96%"
+    document.getElementById('paniniFull').style.height = "88%";
+  }
 };
 
 document.getElementById('panini').addEventListener('click', paniniClick, false);
+
+  //Menu Mid
 
 //---Sandwiches---
 
@@ -271,10 +276,19 @@ document.getElementById('sandwich').addEventListener('mouseout', sandwichMouseOu
     //Click
 
 function sandwichClick() {
-  document.getElementById('sandwich-full').style.top = "16px"; 
-  document.getElementById('sandwich-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('sandwich-full').style.height = "96%"
-  document.getElementById('sandwichFull').style.height = "88%";
+  if(screenSize <= 770) {
+    document.getElementById('sandwich-full').style.top = "16px"; 
+    document.getElementById('sandwich-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('sandwich-full').style.height = "96%"
+    document.getElementById('sandwichFull').style.height = "88%";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('sandwich-full').style.top = "16px"; 
+    document.getElementById('sandwich-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('sandwich-full').style.height = "96%"
+    document.getElementById('sandwichFull').style.height = "88%";
+  }
 };
 
 document.getElementById('sandwich').addEventListener('click', sandwichClick, false);
@@ -303,9 +317,17 @@ document.getElementById('soup-salad').addEventListener('mouseout', soupSaladMous
     //Click
 
 function soupSaladClick() {
-  document.getElementById('soup-salad-full').style.top = "16px"; 
-  document.getElementById('soup-salad-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('soupSaladFull').style.height = "32rem";
+  if(screenSize <= 770) {
+    document.getElementById('soup-salad-full').style.top = "16px"; 
+    document.getElementById('soup-salad-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('soupSaladFull').style.height = "32rem";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('soup-salad-full').style.top = "16px"; 
+    document.getElementById('soup-salad-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('soupSaladFull').style.height = "32rem";
+  }
 };
 
 document.getElementById('soup-salad').addEventListener('click', soupSaladClick, false);
@@ -334,12 +356,22 @@ document.getElementById('savory-crepes').addEventListener('mouseout', savoryCrep
     //Click
 
 function savoryCrepesClick() {
-  document.getElementById('savory-crepes-full').style.top = "16px"; 
-  document.getElementById('savory-crepes-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('savoryCrepesFull').style.height = "32rem";
+  if(screenSize <= 770) {
+    document.getElementById('savory-crepes-full').style.top = "16px"; 
+    document.getElementById('savory-crepes-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('savoryCrepesFull').style.height = "32rem";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('savory-crepes-full').style.top = "16px"; 
+    document.getElementById('savory-crepes-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('savoryCrepesFull').style.height = "32rem";
+  }
 };
 
 document.getElementById('savory-crepes').addEventListener('click', savoryCrepesClick, false);
+
+  //Menu Bottom
 
 //---Sweet Crepes---
 
@@ -366,10 +398,19 @@ document.getElementById('sweet-crepes').addEventListener('mouseout', sweetCrepes
     //Click
 
 function sweetCrepesClick() {
-  document.getElementById('sweet-crepes-full').style.top = "16px"; 
-  document.getElementById('sweet-crepes-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('sweet-crepes-full').style.height = "96%"
-  document.getElementById('sweetCrepesFull').style.height = "88%";
+  if(screenSize <= 770) {
+    document.getElementById('sweet-crepes-full').style.top = "16px"; 
+    document.getElementById('sweet-crepes-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('sweet-crepes-full').style.height = "96%"
+    document.getElementById('sweetCrepesFull').style.height = "88%";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('sweet-crepes-full').style.top = "16px"; 
+    document.getElementById('sweet-crepes-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('sweet-crepes-full').style.height = "96%"
+    document.getElementById('sweetCrepesFull').style.height = "88%";
+  }
 };
 
 document.getElementById('sweet-crepes').addEventListener('click', sweetCrepesClick, false);
@@ -399,10 +440,19 @@ document.getElementById('kids').addEventListener('mouseout', kidsMouseOut, false
     //Click
 
 function kidsClick() {
-  document.getElementById('kids-full').style.top = "16px"; 
-  document.getElementById('kids-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('kids-full').style.height = "96%"
-  document.getElementById('kidsFull').style.height = "88%";
+  if(screenSize <= 770) {
+    document.getElementById('kids-full').style.top = "16px"; 
+    document.getElementById('kids-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('kids-full').style.height = "96%"
+    document.getElementById('kidsFull').style.height = "88%";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('kids-full').style.top = "16px"; 
+    document.getElementById('kids-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('kids-full').style.height = "96%"
+    document.getElementById('kidsFull').style.height = "88%";
+  }
 };
 
 document.getElementById('kids').addEventListener('click', kidsClick, false);
@@ -432,9 +482,17 @@ document.getElementById('drink').addEventListener('mouseout', drinkMouseOut, fal
     //Click
 
 function drinkClick() {
-  document.getElementById('drink-full').style.top = "16px"; 
-  document.getElementById('drink-full').style.transform = "translate(-50%, 0)";
-  document.getElementById('drinkFull').style.height = "32rem";
+  if(screenSize <= 770) {
+    document.getElementById('drink-full').style.top = "16px"; 
+    document.getElementById('drink-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('drinkFull').style.height = "32rem";
+    autoScrollTo('fullMenuLink');
+  }
+  else {
+    document.getElementById('drink-full').style.top = "16px"; 
+    document.getElementById('drink-full').style.transform = "translate(-50%, 0)";
+    document.getElementById('drinkFull').style.height = "32rem";
+  }
 };
 
 document.getElementById('drink').addEventListener('click', drinkClick, false);
@@ -476,3 +534,41 @@ google.maps.event.addDomListener(window, 'load', init_map);
   }
 }
 (document,"script","twitter-wjs");
+
+//--------------------Scroll To Top----------------------
+
+function scrollTop() {
+  resetScroller('sectionOne');
+};
+
+document.getElementById('toTop').addEventListener('click', scrollTop, false);
+
+//--------------------Site Nav---------------------------
+
+  //---Nav One---
+
+document.getElementById('siteNav1').addEventListener('click', scrollTop, false);
+
+  //---Nav Two---
+
+function navTwoScroll() {
+  resetScroller('menuSection');
+};
+
+document.getElementById('siteNav2').addEventListener('click', navTwoScroll, false);
+
+  //---Nav Three---
+
+function navThreeScroll() {
+  resetScroller('find-us-link');
+};
+
+document.getElementById('siteNav3').addEventListener('click', navThreeScroll, false);
+
+  //---Nav Four---
+
+function navFourScroll() {
+  resetScroller('about-link');
+};
+
+document.getElementById('siteNav4').addEventListener('click', navFourScroll, false);
