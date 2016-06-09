ad0455e41d868e84f5aa3544e2a5d5a493a9bf88
depth23 <deon.p.sharps@gmail.com>
2016-06-09 13:34:25 -0600

    server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = 8765;
 
-app.use(express.static('../public'));  
+app.use(express.static(path.join(__dirname, '../public')));  
 
-app.get('/', function(req, res) {
-    res.send('../public/index.html');
-});
+//app.get('/', function(req, res) {
+//    res.send('../public/index.html');
+//});
 
 var server = app.listen(process.env.PORT || port, function () {
     console.log('express server listening on port ' + port)
