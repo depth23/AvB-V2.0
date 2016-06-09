8090a559609668fa42d3ac7ed900b08409969b7b
depth23 <deon.p.sharps@gmail.com>
2016-06-08 23:16:17 -0600

    server file

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ var port = 8765;
 app.use(express.static('../public'));  
 
 app.get('/', function(req, res) {
-//    res.send('../public/index.html');
-  res.send('GET request to the homepage');
+    res.send('../public/index.html');
 });
 
 var server = app.listen(process.env.PORT || port, function () {
