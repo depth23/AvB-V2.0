6ef99e011a06b6e8abf27e39fee4513202e06867
depth23 <deon.p.sharps@gmail.com>
2016-06-08 23:00:00 -0600

    uncommented the server

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ var port = 8765;
 
 app.use(express.static('../public'));  
 
-//app.get('/', function(req, res) {
-//    res.send('../public/index.html');
-//});
+app.get('/', function(req, res) {
+    res.send('../public/index.html');
+});
 
 var server = app.listen(process.env.PORT || port, function () {
     console.log('express server listening on port ' + port)
